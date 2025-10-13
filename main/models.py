263820ec8ed8aa661dd4b8b92afe567c10880fa4
Mehdi Mohseni <wuka.sec@gmail.com>
2025-10-13 16:02:49 -0400

    remove autoload news and make it manually

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from bs4 import BeautifulSoup
 
+
 class News(models.Model):
     url = models.CharField(max_length=200)
     content = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,8 +1,6 @@
 import requests
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from .models import News
-
-API_URL = 'https://fast-api-7gqj.onrender.com'
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -16,19 +14,9 @@ class AboutUsView(TemplateView):
     template_name = 'about_us.html'
 
 
-class NewsView(TemplateView):
+class NewsView(ListView):
+    model = News
     template_name = 'news.html'
-
-    def get_context_data(self, **kwargs):
-        url_list = requests.get(API_URL).json().get('urls', [])
-
-        for url in url_list:
-            if not News.objects.filter(url=url).exists():
-                News.objects.create(url=url, is_translated=False)
-
-        context = super().get_context_data(**kwargs)
-        context["context"] = News.objects.all()
-        return context
 
 
 class NewsDetailView(DetailView):
